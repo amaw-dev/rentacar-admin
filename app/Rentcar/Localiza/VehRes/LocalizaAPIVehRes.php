f09d7d440568217d2f3af5d1774f32c3533b5ca0
Pablo Diaz <pabloandi@gmail.com>
2024-09-20 15:08:43 -0500

    move xml definition to blade template

--- app/Rentcar/Localiza/VehRes/LocalizaAPIVehRes.php
+++ app/Rentcar/Localiza/VehRes/LocalizaAPIVehRes.php
@@ -4,12 +4,14 @@ namespace App\Rentcar\Localiza\VehRes;
 
 use App\Rentcar\Localiza\Contracts\LocalizaAPIRequest;
 use App\Rentcar\Localiza\Exceptions\TimeoutException;
+use App\Rentcar\Localiza\Exceptions\UnknowException;
 use App\Rentcar\Localiza\Exceptions\VehRes\NoReservationStatusException;
 use App\Rentcar\Localiza\Exceptions\VehRes\NoReserveCodeException;
 use App\Rentcar\Localiza\ProcessWarning;
 use App\Rentcar\Localiza\LocalizaAPI;
 use App\Rentcar\Localiza\VehRes\VehRes;
 use App\Traits\MultipleAttributeSetter;
+use Illuminate\Http\Client\ConnectionException;
 use SimpleXMLElement;
 
 class LocalizaAPIVehRes extends LocalizaAPI implements LocalizaAPIRequest {
@@ -86,44 +88,8 @@ class LocalizaAPIVehRes extends LocalizaAPI implements LocalizaAPIRequest {
     }
 
     public function getFilledInputXML() : string {
-        return <<<XML
-                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:ns="http://www.opentravel.org/OTA/2003/05">
-                    <soapenv:Body>
-                        <tem:OTA_VehRes xmlns="http://tempuri.org/">
-                            <tem:OTA_VehResRQ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" EchoToken="{$this->token}" Target="Test" PrimaryLangID="por">
-                                <tem:POS>
-                                    <tem:Source ISOCountry="CO">
-                                        <ns:RequestorID ID="{$this->requestorID}" Type="5" xmlns="http://www.opentravel.org/OTA/2003/05" />
-                                    </tem:Source>
-                                </tem:POS>
-                                <tem:VehResRQCore xmlns="http://tempuri.org/">
-                                    <ns:VehRentalCore PickUpDateTime="{$this->pickupDateTime}" ReturnDateTime="{$this->returnDateTime}" xmlns="http://www.opentravel.org/OTA/2003/05">
-                                        <ns:PickUpLocation LocationCode="{$this->pickupLocation}" CodeContext="internal code"/>
-                                        <ns:ReturnLocation LocationCode="{$this->returnLocation}" CodeContext="internal code"/>
-                                    </ns:VehRentalCore>
-                                    <ns:Customer>
-                                        <ns:Primary>
-                                            <ns:PersonName>
-                                                <ns:Surname>{$this->name}</ns:Surname>
-                                            </ns:PersonName>
-                                            <ns:Email>{$this->email}</ns:Email>
-                                            <ns:CitizenCountryName Code="CO" />
-                                            <ns:Telephone PhoneNumber="{$this->phoneNumber}"/>
-                                        </ns:Primary>
-                                    </ns:Customer>
-                                    <ns:VehPref CodeContext="internal code" Code="{$this->category}" />
-                                    <ns:RateQualifier RateQualifier="{$this->rateQualifier}" />
-                                </tem:VehResRQCore>
-                                <tem:VehResRQInfo>
-                                    <ns:RentalPaymentPref PaymentType="2">
-                                        <ns:Voucher ValueType="2" />
-                                    </ns:RentalPaymentPref>
-                                    <ns:Reference Type="41" ID="{$this->referenceToken}"></ns:Reference>
-                                </tem:VehResRQInfo>
-                            </tem:OTA_VehResRQ>
-                        </tem:OTA_VehRes>
-                    </soapenv:Body>
-                </soapenv:Envelope>
-            XML;
+        $data = array_merge($this->attributes, $this->getAgencyIdentificationData());
+
+        return view('localiza.inputs.vehres', $data)->render();
     }
 }
