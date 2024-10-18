17b6352b0c864ec1c307103ad71ba9839bb84538
Pablo Diaz <pabloandi@gmail.com>
2024-10-18 16:04:07 -0500

    remove some space of the ID attribute

--- resources/views/localiza/inputs/vehretres.blade.php
+++ resources/views/localiza/inputs/vehretres.blade.php
@@ -4,7 +4,7 @@
         <OTA_VehRetRes xmlns="http://tempuri.org/">
             <OTA_VehRetResRQ RetransmissionIndicator="false" TransactionStatusCode="Start" Target="Test" TimeStamp="0001-01-01T00:00:00" EchoToken="{{ $token }}" Version="0">
                 <VehRetResRQCore>
-                    <UniqueID xmlns="http://www.opentravel.org/OTA/2003/05"             ID="{{ $reserve_code }}" Type="14" />
+                    <UniqueID xmlns="http://www.opentravel.org/OTA/2003/05" ID="{{ $reserve_code }}" Type="14" />
                 </VehRetResRQCore>
             </OTA_VehRetResRQ>
         </OTA_VehRetRes>
